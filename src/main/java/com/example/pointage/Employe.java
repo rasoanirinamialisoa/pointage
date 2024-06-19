b46afd9a52e4747e63b8f82aa2d5743659c6b33e
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-06-19 12:48:10 +0300

    feat: add methode calculerHeuresSupplemetairesEtSalaire

--- src/main/java/com/example/pointage/Employe.java
+++ src/main/java/com/example/pointage/Employe.java
@@ -30,4 +30,23 @@ public class Employe {
         this.salaireNet = salaireBrut * 0.8;
     }
 
+    public double calculerHeuresSupplementairesEtSalaire(double heuresTravaillees) {
+        double heuresNormales = categorie.getHeuresNormalesParSemaine();
+        double tauxHoraireNormal = categorie.getSalaireParSemaine() / heuresNormales;
+
+        double heuresSupp = Math.max(0, heuresTravaillees - heuresNormales);
+        double heuresSuppPayees = Math.min(heuresSupp, 20);
+
+        double salaireSupp = 0.0;
+
+        if (heuresSuppPayees > 0) {
+            double heuresHS30 = Math.min(8, heuresSuppPayees);
+            double heuresHS50 = Math.min(12, Math.max(0, heuresSuppPayees - 8));
+
+            salaireSupp += heuresHS30 * tauxHoraireNormal * 1.3;
+            salaireSupp += heuresHS50 * tauxHoraireNormal * 1.5;
+        }
+
+        return salaireSupp;
+    }
 }
